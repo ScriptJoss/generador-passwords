ab31d7c18cdf535202118070cf90d0853ab1c1f3
Jose <josesitocrack0429p@gmail.com>
2024-12-18 16:54:31 -0500

    🥳 Completado

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+# Importamos libreria
 import random
 
+# Guardamos todos los caracteres
 passwords_caract = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
+# Pedimos los caracteres
 pedir_passwords = int(input('Ingrese la longitud de la contraseña: '))
 
+# Guardamos para agregar aca los caracteres
 psw = ""
 
+# Recorre la cantidad de caracteres que el usuario puso para luego posteriormente con random.choice elegir caracteres randoms (aumentando) y al final guardandolos en "psw"
 for i in range(pedir_passwords):
     psw += random.choice(passwords_caract)
 
+# Imprimos los caracteres finales 
 print(psw)
